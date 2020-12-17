c2577ffc0ca70bffaceb9873951f36dd84a45492
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-12-17 11:21:37 +1100

    added code to service-worker.js based on examples in week 18

---
+++ public/service-worker.js
@@ -0,0 +1,57 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/db.js",
+    "/index.html",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/styles.css"
+  ];
+  
+  const PRECACHE = "precache-v1";
+  const RUNTIME = "runtime";
+
+  // install
+  self.addEventListener("install", event => {
+    event.waitUntil(
+      caches.open(PRECACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
+  
+  // activate
+  self.addEventListener("activate", event => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+      }).then(cachesToDelete => {
+        return Promise.all(cachesToDelete.map(cacheToDelete => {
+          return caches.delete(cacheToDelete);
+        }));
+      }).then(() => self.clients.claim())
+    );
+  });
+  
+  // fetch
+  self.addEventListener("fetch", event => {
+    if (event.request.url.startsWith(self.location.origin)) {
+      event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+          // clone the response and store it in the cache
+          return caches.open(RUNTIME).then(cache => {
+            return fetch(event.request).then(response => {
+              return cache.put(event.request, response.clone()).then(() => {
+                return response;
+              });
+            });
+          });
+        })
+      );
+    }
+  });
